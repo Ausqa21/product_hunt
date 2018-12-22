67957c3745abe742ab1b8102cb2c893769adb445
Godric Oscar Dolphine <ausqa95@gmail.com>
2018-12-22 08:14:08 +0000

    Initial commit

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    pub_date = models.DateField(auto_now=True)
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='icons/')
+    body = models.TextField()
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
